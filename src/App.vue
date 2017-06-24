3b4e16127dc0642fe55669baa550d73ff3d10122
nicelj <nicelj92@gmail.com>
2017-06-24 22:12:30 +0800

    dev

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
     <div>
         <Top></Top>
         <router-view></router-view>
+        <Bottom></Bottom>
     </div>
 </template>
 <script>
-import Top from './components/header';
-    export default{
-        components:{
-            Top
+    import Top from './components/header';
+    import Bottom from './components/footer';
+    export default {
+        components: {
+            Top,Bottom
         }
     }
 </script>
